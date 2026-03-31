1c756583be4efe37b3a6fb4494643629a54e5dc8
HooriyaZahid <hooriyajm23@gmail.com>
2026-03-31 23:24:18 +0500

    Add data download script

---
+++ download_data.py
@@ -0,0 +1,15 @@
+﻿from datasets import load_dataset
+import pandas as pd
+
+dataset = load_dataset("google-research-datasets/go_emotions", "simplified")
+
+train = dataset["train"].to_pandas()
+val = dataset["validation"].to_pandas()
+test = dataset["test"].to_pandas()
+
+train.to_csv("train.csv", index=False)
+val.to_csv("val.csv", index=False)
+test.to_csv("test.csv", index=False)
+
+print("Done! Shape:", train.shape)
+print(train.head())
